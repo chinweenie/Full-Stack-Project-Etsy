e4f784ce74e2834c8e3aecf85bd82192abc86f2e
Chin Wee Nie <chinweenie@gmail.com>
2019-09-04 12:49:16 +0800

    Add active storage table

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,6 +19,8 @@ class Product < ApplicationRecord
     belongs_to :shop 
     belongs_to :category
 
+    has_many_attached :images
+
     has_many :favorites, as: :favoritable, dependent: :destroy
     has_many :users_who_favorited_me, through: :favorites
 
