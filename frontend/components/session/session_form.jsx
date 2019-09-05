36f9e3bb837fef4713fba84214063d4d3698ecb1
Chin Wee Nie <chinweenie@gmail.com>
2019-09-06 06:42:09 +0800

    Fix errors styling

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -8,8 +8,12 @@ class SessionForm extends React.Component {
             password: '',
             email: ''
         }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDemo = this.handleDemo.bind(this);
+        this.handleSubmit = this
+            .handleSubmit
+            .bind(this);
+        this.handleDemo = this
+            .handleDemo
+            .bind(this);
     };
 
     handleDemo(event) {
@@ -19,18 +23,24 @@ class SessionForm extends React.Component {
             email: 'chinwn@gmail.com',
             password: 'hunter12'
         });
-        this.props.login(demo).then(this.props.closeModal());
+        this
+            .props
+            .login(demo)
+            .then(this.props.closeModal());
     }
 
     handleSubmit(event) {
         event.preventDefault();
         const user = Object.assign({}, this.state); // {user: {fname, password}}
-        this.props.processForm(user).then(this.props.closeModal);
+        this
+            .props
+            .processForm(user)
+            .then(this.props.closeModal);
     }
 
     update(field) {
         return (event) => {
-            this.setState({ [field]: event.target.value });
+            this.setState({[field]: event.target.value});
         }
     }
 
@@ -60,7 +70,6 @@ class SessionForm extends React.Component {
         const registerHeader = (
             <div className="register-header">
                 <h1>Create your account</h1>
-                <p>Registration is easy.</p>
             </div>
         );
 
@@ -68,67 +77,73 @@ class SessionForm extends React.Component {
             ? registerHeader
             : loginHeader;
 
-        const fnameInput = this.props.formType === 'Register' ? (
-            <div className="login-form">
-                <label htmlFor="fname">
-                    First name
-                    <span className="text-orange">*</span>
-                </label>
+        const fnameInput = this.props.formType === 'Register'
+            ? (
+                <div className="login-form">
+                    <label htmlFor="fname">
+                        First name
+                        <span className="text-orange">*</span>
+                    </label>
 
-                <input
-                    type="text"
-                    value={this.state.fname}
-                    onChange={this.update('fname')}
-                    className="login-input"
-                    id="fname" />
+                    <input
+                        type="text"
+                        value={this.state.fname}
+                        onChange={this.update('fname')}
+                        className="login-input"
+                        id="fname"/>
 
-                <br />
-            </div>
-        ) : ('');
+                    <br/>
+                </div>
+            )
+            : ('');
 
         return (
-            <div className="login-form-container">
+            <div className="session-form">
+                <div className="errors">
+                    {this.renderErrors()}
+                </div>
+                <div className="login-form-container">
 
-                <form onSubmit={this.handleSubmit} className="login-form-box">
+                    <form onSubmit={this.handleSubmit} className="login-form-box">
 
-                    {header}
+                        {header}
 
-                    <div className="login-form">
-                        <br />
+                        <div className="login-form">
+                            <br/>
 
-                        <label htmlFor="email">Email address
-                            <span className="text-orange">*</span>
-                        </label>
-                        <input
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.update('email')}
-                            className="login-input"
-                            id="email" />
-                        {this.renderErrors()}
-                        <br />
+                            <label htmlFor="email">Email address
+                                <span className="text-orange">*</span>
+                            </label>
+                            <input
+                                type="email"
+                                value={this.state.email}
+                                onChange={this.update('email')}
+                                className="login-input"
+                                id="email"/>
 
-                        {fnameInput}
+                            <br/> {fnameInput}
 
-                        <label htmlFor="password">Password
-                            <span className="text-orange">*</span>
-                        </label>
-                        <input
-                            type="password"
-                            value={this.state.password}
-                            onChange={this.update('password')}
-                            className="login-input"
-                            id="password" />
+                            <label htmlFor="password">Password
+                                <span className="text-orange">*</span>
+                            </label>
+                            <input
+                                type="password"
+                                value={this.state.password}
+                                onChange={this.update('password')}
+                                className="login-input"
+                                id="password"/>
 
-                        <br />
+                            <br/>
 
+                            <button className="clicky">{this.props.formType}</button>
 
-                        <button className="clicky">{this.props.formType}</button>
+                        </div>
+                    </form>
+                    <button onClick={this.handleDemo} className="clicky" id="demo-login">Demo login</button>
+                </div>
 
-                    </div>
-                </form>
-                <button onClick={this.handleDemo} className="clicky">Demo login</button>
             </div>
+
         );
     }
 
