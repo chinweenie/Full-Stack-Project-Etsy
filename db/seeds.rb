e35e055e37c76a42adcda003be853d22946eefbb
Chin Wee Nie <chinweenie@gmail.com>
2019-09-10 00:33:02 +0800

    Reseed category for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
 User.create!(email: 'admin@epsy.com', fname: 'admin', password: 'hunter12')
+
+Category.create!(name: 'Jewelry & Accessories')
+Category.create!(name: 'Clothing & Shoes')
+Category.create!(name: 'Home & Living')
+Category.create!(name: 'Wedding & Party')
+Category.create!(name: 'Toys & Entertainment')
+Category.create!(name: 'Art & Collectibles')
+Category.create!(name: 'Craft Supplies & Tools')
+Category.create!(name: 'Vintage')
