5a15769024ec318b3b2d84e57a1a1131b4b04468
Chin Wee Nie <chinweenie@gmail.com>
2019-09-12 08:27:48 +0800

    Fix product show styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -46,8 +46,8 @@ class Navbar extends React.Component{
         return (
             <div className="navbar">
                 {component}
-                {development}
-                {/* {production} */}
+                {/* {development} */}
+                {production}
             </div>
         );
     };
