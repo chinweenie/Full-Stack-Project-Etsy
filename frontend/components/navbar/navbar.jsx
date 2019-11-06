ea7147059093b7866f061db06fc1b19960628d43
Chin Wee Nie <chinweenie@gmail.com>
2019-11-06 09:10:26 +0800

    change styling for demo login

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -47,8 +47,8 @@ class Navbar extends React.Component{
         return (
             <div className="navbar">
                 {component}
-                {development}
-                {/* {production} */}
+                {/* {development} */}
+                {production}
             </div>
         );
     };
